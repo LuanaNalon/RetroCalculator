9e58e6ed0823bfb130ece2de4edf0eb9f518bb00
Luana Nalon <luananalon@hotmail.com>
2021-04-30 16:12:58 +0100

    Sounds added on buttons and font change

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
     }
-
+    @IBAction func numberPressed(sender: UIButton){
+        playSound()
+    }
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
 
 }
 
